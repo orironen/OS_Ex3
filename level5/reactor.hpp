536aa70cfe6d86640fcced85ca6dbeeabab74636
e10withadot <e10withadot@gmail.com>
2025-07-25 13:43:10 +0300

    added reactor header (level5)

---
+++ level5/reactor.hpp
@@ -0,0 +1,10 @@
+typedef void * (* reactorFunc)(int fd);
+typedef void reactor;
+// starts new reactor and returns pointer to it
+void * startReactor();
+// adds fd to Reactor (for reading) ; returns 0 on success.
+int addFdToReactor(void * reactor, int fd, reactorFunc func); int addFdToReactor(void * reactor, int fd, reactorFunc func);
+// removes fd from reactor
+int removeFdFromReactor(void * reactor, int fd);
+// stops reactor
+int stopReactor(void * reactor);
