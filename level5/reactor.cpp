e4eb9fc39f1e73bce835848f0050a79097fc73ad
e10withadot <e10withadot@gmail.com>
2025-07-29 14:01:51 +0300

    fixed linker errors

--- level5/reactor.cpp
+++ level5/reactor.cpp
@@ -1,85 +1,86 @@
 #include "reactor.hpp"
-using namespace reactor;
 #include <iostream>
 #include <algorithm>
 
-void *pollThread(Reactor *r)
-{
-    while (r->running)
+namespace reactor {
+    void *pollThread(Reactor *r)
     {
-        int p = poll(r->fds.data(), r->fds.size(), 1000);
-        if (p < 0)
+        while (r->running)
         {
-            std::cerr << "Poll error" << std::endl;
-            continue;
-        }
-        // בודק אילו file descriptors מוכנים לקריאה
-        for (auto &pfd : r->fds)
-        {
-            if (pfd.revents & POLLIN)
+            int p = poll(r->fds.data(), r->fds.size(), 1000);
+            if (p < 0)
             {
-                auto handler = r->handlers[pfd.fd];
-                if (handler)
+                std::cerr << "Poll error" << std::endl;
+                continue;
+            }
+            // בודק אילו file descriptors מוכנים לקריאה
+            for (auto &pfd : r->fds)
+            {
+                if (pfd.revents & POLLIN)
                 {
-                    handler(pfd.fd);
+                    auto handler = r->handlers[pfd.fd];
+                    if (handler)
+                    {
+                        handler(pfd.fd);
+                    }
                 }
             }
         }
+        return nullptr;
     }
-    return nullptr;
-}
 
-void* startReactor()
-{
-    Reactor *r = new Reactor();
-    r->running = true;
-    r->poll_thread = std::thread(pollThread, r);
-    return static_cast<void *>(r);
-}
-
-int addFdToReactor(void *reactor, int fd, reactorFunc func)
-{
-    if (!reactor)
-        return -1;
-    Reactor *r = static_cast<Reactor *>(reactor);
-    // הוספת fd חדש למערך ה-poll
-    pollfd pfd;
-    pfd.fd = fd;
-    pfd.events = POLLIN;
-    pfd.revents = 0;
-    r->fds.push_back(pfd);
-    r->handlers[fd] = func;
-    return 0;
-}
-
-int removeFdFromReactor(void *reactor, int fd)
-{
-    if (!reactor)
-        return -1;
-    Reactor *r = static_cast<Reactor *>(reactor);
-    // מחיקת ה-fd מהמערך
-    auto it = std::find_if(r->fds.begin(), r->fds.end(),
-                           [fd](const pollfd &pfd)
-                           { return pfd.fd == fd; });
-    if (it != r->fds.end())
+    void* startReactor()
     {
-        r->fds.erase(it);
-        r->handlers.erase(fd);
+        Reactor *r = new Reactor();
+        r->running = true;
+        r->poll_thread = std::thread(pollThread, r);
+        return static_cast<void *>(r);
+    }
+
+    int addFdToReactor(void *reactor, int fd, reactorFunc func)
+    {
+        if (!reactor)
+            return -1;
+        Reactor *r = static_cast<Reactor *>(reactor);
+        // הוספת fd חדש למערך ה-poll
+        pollfd pfd;
+        pfd.fd = fd;
+        pfd.events = POLLIN;
+        pfd.revents = 0;
+        r->fds.push_back(pfd);
+        r->handlers[fd] = func;
         return 0;
     }
-    return -1;
-}
 
-int stopReactor(void *reactor)
-{
-    if (!reactor)
-        return -1;
-    Reactor *r = static_cast<Reactor *>(reactor);
-    r->running = false;
-    if (r->poll_thread.joinable())
+    int removeFdFromReactor(void *reactor, int fd)
     {
-        r->poll_thread.join(); // main thread waits until t finishes
+        if (!reactor)
+            return -1;
+        Reactor *r = static_cast<Reactor *>(reactor);
+        // מחיקת ה-fd מהמערך
+        auto it = std::find_if(r->fds.begin(), r->fds.end(),
+                               [fd](const pollfd &pfd)
+                               { return pfd.fd == fd; });
+        if (it != r->fds.end())
+        {
+            r->fds.erase(it);
+            r->handlers.erase(fd);
+            return 0;
+        }
+        return -1;
     }
-    delete r;
-    return 0;
+
+    int stopReactor(void *reactor)
+    {
+        if (!reactor)
+            return -1;
+        Reactor *r = static_cast<Reactor *>(reactor);
+        r->running = false;
+        if (r->poll_thread.joinable())
+        {
+            r->poll_thread.join(); // main thread waits until t finishes
+        }
+        delete r;
+        return 0;
+    }
 }
